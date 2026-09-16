85ec568fb6e07e96a9c3fa282fa3aa26913cf00a
Nymur Rahman <nymur.nyar@gmail.com>
2026-09-15 21:41:13 -0600

    Added comment for the rebase example

--- Assets/moveImage.cs
+++ Assets/moveImage.cs
@@ -14,5 +14,7 @@ public class moveImage : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.up * 10f * Time.deltaTime);
+
+        // this is a line of comment i am adding to demo the rebase stuff
     }
 }
